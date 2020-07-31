fbad229ef1c977f245654075b9bb464d9396df43
Hu Tiger <huxy99@yahoo.com>
2020-07-31 15:51:35 -0500

    edit Product feature

--- src/main/java/com/youhubs/ProductController.java
+++ src/main/java/com/youhubs/ProductController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProductController {
@@ -35,6 +37,16 @@ public class ProductController {
 		service.save(product);
 
 		return "redirect:/";
+	}
+
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProduct(@PathVariable(name = "id") Long id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+
+		Product product = service.get(id);
+		mav.addObject("product", product);
+
+		return mav;
 	}
 
 }
