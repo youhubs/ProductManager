2ccc69623173464d68a7e9e949deaad20df2007b
Hu Tiger <huxy99@yahoo.com>
2020-07-31 15:04:37 -0500

    create new product

--- src/main/java/com/youhubs/ProductController.java
+++ src/main/java/com/youhubs/ProductController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
@@ -19,4 +21,20 @@ public class ProductController {
 
 		return "index";
 	}
+
+	@RequestMapping("/new")
+	public String NewProductForm(Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+
+		return "new_product";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		service.save(product);
+
+		return "redirect:/";
+	}
+
 }
